6cfe2ecd9485e830e2f771b1d9f78427634f9ba9
Adam Swanson <adam.swanson.98@gmail.com>
2020-12-08 19:33:41 -0500

    Remove old slides

--- illiana-app/src/app/carousel/carousel.component.ts
+++ illiana-app/src/app/carousel/carousel.component.ts
@@ -17,31 +17,15 @@ export class CarouselComponent implements OnInit {
 
   public slides = [
     {
-      src: "../../assets/img/rake.jpg",
-      header: "Showgrounds Clean-Up Day",
-      body: 'Saturday, December 5 @ 1:00 PM. Bring your rakes and leaf blowers to help clean up the grounds.',
-      link: { href: "/events", text: "View Events" }
-    },
-    {
-      src: "../../assets/img/intro-carousel/tom.jpg",
-      header: "A Message from the President",
-      link: { href: "/letter-september2020", text: "Read Now" }
+      src: "../../assets/img/intro-carousel/1.jpg",
+      header: "Where the Past has a Future!",
+      body: "The Illiana Antique Power Association is a non-profit organization dedicated to the preservation of our early rural way of life and the mechanical marvels that changed farming forever.",
+      link: { href: "/", fragment: "about", text: "Learn More" }
     },
     {
       src: "../../assets/img/history-day.jpg",
       header: "History Day is Going Virtual!",
       body: "This year, we're making History Day into an entirely virtual event where all activities will be done online! More details to come."
-    },
-    {
-      src: "../../assets/img/show/1.jpg",
-      header: "Annual Show: Cancelled",
-      link: { href: "/show-info", text: "View Details" }
-    },
-    {
-      src: "../../assets/img/intro-carousel/1.jpg",
-      header: "Where the Past has a Future!",
-      body: "The Illiana Antique Power Association is a non-profit organization dedicated to the preservation of our early rural way of life and the mechanical marvels that changed farming forever.",
-      link: { href: "/", fragment: "about", text: "Learn More" }
     }
   ]
   currentSlide = 0;
