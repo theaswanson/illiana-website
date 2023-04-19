9cd2da9e0cfc001ff50400ffcc30e0cd0343f3d3
Adam Swanson <adam.swanson.98@gmail.com>
2023-04-18 21:31:57 -0400

    Update contact info on Ladies Activities

--- illiana-app/src/app/components/ladies/ladies.component.ts
+++ illiana-app/src/app/components/ladies/ladies.component.ts
@@ -3,27 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ladies',
   templateUrl: './ladies.component.html',
-  styleUrls: ['./ladies.component.scss']
+  styleUrls: ['./ladies.component.scss'],
 })
 export class LadiesComponent implements OnInit {
-
   images: string[];
 
   constructor() {
     this.images = [
       'assets/img/quilters.jpg',
       'assets/img/quilters2.jpg',
-      'assets/img/quilters3.jpg',
       'assets/img/quilters4.jpg',
       'assets/img/sewlongago.jpg',
       'assets/img/sewlongago2.jpg',
       'assets/img/kitchen.jpg',
       'assets/img/kitchen2.jpg',
-      'assets/img/kitchen3.jpg'
-    ]
+      'assets/img/kitchen3.jpg',
+      'assets/img/quilters3.jpg',
+    ];
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
