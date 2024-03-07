7a73cd9dd4852d4526b8afcabc7e356c48e29764
Adam Swanson <adam.swanson.98@gmail.com>
2024-03-07 18:09:42 -0500

    Refactor events

--- illiana-app/src/app/components/events/events.component.ts
+++ illiana-app/src/app/components/events/events.component.ts
@@ -1,58 +1,106 @@
 import { Component, OnInit } from '@angular/core';
 import events from './events.json';
 
-class Event {
-  year: number;
+interface EventJson {
   title: string;
-  date: string;
-  time?: string;
+  startDate: string;
+  endDate?: string;
   location: string;
   special?: boolean;
+  allDay?: boolean;
+}
+
+interface Event {
+  title: string;
+  startDate: Date;
+  endDate?: Date;
+  location: string;
+  special?: boolean;
+  allDay?: boolean;
 }
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-
   events: Event[];
-  years: number[];
-  eventsMap = new Map<number, any[]>();
+  eventsByYear = new Map<number, any[]>();
+  dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    timeZone: 'America/New_York',
+  };
+  timeOptions: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: '2-digit',
+    timeZone: 'America/New_York',
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.events = events;
-    this.events = this.sortEvents(this.events);
-    this.years = this.getYears(this.events);
-    this.populateEventsMap(this.years, this.eventsMap, this.events);
+    this.events = this.sortEvents(this.mapEvents(events));
+    const years = this.getYears(this.events);
+    this.populateEventsMap(years, this.eventsByYear, this.events);
   }
 
-  private sortEvents(events: any[]): any[] {
+  private toDateRangeString(event: Event): string {
+    const startDateString = this.toDateString(event.startDate);
+
+    return !event.endDate
+      ? startDateString
+      : `${startDateString} - ${this.toDateString(event.endDate)}`;
+  }
+
+  private toDateString(date: Date): string {
+    return date.toLocaleString('en-US', this.dateOptions);
+  }
+
+  private toTimeString(date: Date): string {
+    return date.toLocaleString('en-US', this.timeOptions);
+  }
+
+  private mapEvents(events: EventJson[]): Event[] {
+    return events.map((event) => ({
+      title: event.title,
+      startDate: new Date(event.startDate),
+      endDate: event.endDate && new Date(event.endDate),
+      location: event.location,
+      special: event.special,
+      allDay: event.allDay,
+    }));
+  }
+
+  private sortEvents(events: Event[]): Event[] {
     return events.sort((a, b) => {
-      if (a.year > b.year) return -1;
-      if (a.year < b.year) return 1;
+      return a.startDate > b.startDate ? 1 : -1;
     });
   }
 
-  private getYears(events: any[]): number[] {
+  private getYears(events: Event[]): number[] {
     let years = [];
-    events.map(x => {
-      if (years.findIndex(year => year === x.year) === -1) {
-        years.push(x.year);
+    events.map((event) => {
+      const eventYear = event.startDate.getFullYear();
+      if (years.findIndex((year) => year === eventYear) === -1) {
+        years.push(eventYear);
       }
     });
     return years;
   }
 
-  private populateEventsMap(years: number[], eventsMap: Map<number, any[]>, events: any[]) {
-    years.map(year => {
-      eventsMap.set(
+  private populateEventsMap(
+    years: number[],
+    eventsByYear: Map<number, Event[]>,
+    events: Event[]
+  ) {
+    years.map((year) => {
+      eventsByYear.set(
         year,
-        events.filter(event => event.year === year)
+        events.filter((event) => event.startDate.getFullYear() === year)
       );
-    })
+    });
   }
 }
