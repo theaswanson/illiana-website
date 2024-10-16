01ff2d13207213ec1043ff1fe44930d9adcb2610
mawalou14 <awaloum213@gmail.com>
2024-10-16 22:48:51 +0100

    Fix spelling errors

--- illiana-app/src/assets/js/main.js
+++ illiana-app/src/assets/js/main.js
@@ -2,51 +2,55 @@
   "use strict";
 
   $("#header-placeholder").load("header.html", function () {
-    $(this).children(':first').unwrap();
+    $(this).children(":first").unwrap();
     // Initiate the wowjs animation library
     new WOW().init();
 
     // Initiate superfish on nav menu
-    $('.nav-menu').superfish({
+    $(".nav-menu").superfish({
       animation: {
-        opacity: 'show'
+        opacity: "show",
       },
-      speed: 400
+      speed: 400,
     });
 
     // Mobile Navigation
-    if ($('#nav-menu-container').length) {
-      var $mobile_nav = $('#nav-menu-container').clone().prop({
-        id: 'mobile-nav'
+    if ($("#nav-menu-container").length) {
+      var $mobile_nav = $("#nav-menu-container").clone().prop({
+        id: "mobile-nav",
       });
-      $mobile_nav.find('> ul').attr({
-        'class': '',
-        'id': ''
+      $mobile_nav.find("> ul").attr({
+        class: "",
+        id: "",
       });
-      $('body').append($mobile_nav);
-      $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
-      $('body').append('<div id="mobile-body-overly"></div>');
-      $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+      $("body").append($mobile_nav);
+      $("body").prepend(
+        '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>'
+      );
+      $("body").append('<div id="mobile-body-overlay"></div>');
+      $("#mobile-nav")
+        .find(".menu-has-children")
+        .prepend('<i class="fa fa-chevron-down"></i>');
 
-      $(document).on('click', '.menu-has-children i', function (e) {
-        $(this).next().toggleClass('menu-item-active');
-        $(this).nextAll('ul').eq(0).slideToggle();
+      $(document).on("click", ".menu-has-children i", function (e) {
+        $(this).next().toggleClass("menu-item-active");
+        $(this).nextAll("ul").eq(0).slideToggle();
         $(this).toggleClass("fa-chevron-up fa-chevron-down");
       });
 
-      $(document).on('click', '#mobile-nav-toggle', function (e) {
-        $('body').toggleClass('mobile-nav-active');
-        $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-        $('#mobile-body-overly').toggle();
+      $(document).on("click", "#mobile-nav-toggle", function (e) {
+        $("body").toggleClass("mobile-nav-active");
+        $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+        $("#mobile-body-overlay").toggle();
       });
 
       $(document).click(function (e) {
         var container = $("#mobile-nav, #mobile-nav-toggle");
         if (!container.is(e.target) && container.has(e.target).length === 0) {
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-            $('#mobile-body-overly').fadeOut();
+          if ($("body").hasClass("mobile-nav-active")) {
+            $("body").removeClass("mobile-nav-active");
+            $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+            $("#mobile-body-overlay").fadeOut();
           }
         }
       });
@@ -55,33 +59,41 @@
     }
 
     // Smooth scroll for the menu and links with .scrollto classes
-    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function () {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    $(".nav-menu a, #mobile-nav a, .scrollto").on("click", function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
         var target = $(this.hash);
         if (target.length) {
           var top_space = 0;
 
-          if ($('#header').length) {
-            top_space = $('#header').outerHeight();
+          if ($("#header").length) {
+            top_space = $("#header").outerHeight();
 
-            if (!$('#header').hasClass('header-fixed')) {
+            if (!$("#header").hasClass("header-fixed")) {
               top_space = top_space - 20;
             }
           }
 
-          $('html, body').animate({
-            scrollTop: target.offset().top - top_space
-          }, 1500, 'easeInOutExpo');
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top - top_space,
+            },
+            1500,
+            "easeInOutExpo"
+          );
 
-          if ($(this).parents('.nav-menu').length) {
-            $('.nav-menu .menu-active').removeClass('menu-active');
-            $(this).closest('li').addClass('menu-active');
+          if ($(this).parents(".nav-menu").length) {
+            $(".nav-menu .menu-active").removeClass("menu-active");
+            $(this).closest("li").addClass("menu-active");
           }
 
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-            $('#mobile-body-overly').fadeOut();
+          if ($("body").hasClass("mobile-nav-active")) {
+            $("body").removeClass("mobile-nav-active");
+            $("#mobile-nav-toggle i").toggleClass("fa-times fa-bars");
+            $("#mobile-body-overlay").fadeOut();
           }
           return false;
         }
@@ -91,9 +103,9 @@
     // Header scroll class
     $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
-        $('#header').addClass('header-scrolled');
+        $("#header").addClass("header-scrolled");
       } else {
-        $('#header').removeClass('header-scrolled');
+        $("#header").removeClass("header-scrolled");
       }
     });
   });
@@ -101,65 +113,95 @@
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
+      $(".back-to-top").fadeIn("slow");
     } else {
-      $('.back-to-top').fadeOut('slow');
+      $(".back-to-top").fadeOut("slow");
     }
   });
-  $('.back-to-top').click(function () {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
+  $(".back-to-top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      "easeInOutExpo"
+    );
     return false;
   });
 
   // Intro carousel
   var introCarousel = $(".carousel");
   var introCarouselIndicators = $(".carousel-indicators");
-  introCarousel.find(".carousel-inner").children(".carousel-item").each(function (index) {
-    (index === 0) ?
-    introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>"):
-      introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
+  introCarousel
+    .find(".carousel-inner")
+    .children(".carousel-item")
+    .each(function (index) {
+      index === 0
+        ? introCarouselIndicators.append(
+            "<li data-target='#introCarousel' data-slide-to='" +
+              index +
+              "' class='active'></li>"
+          )
+        : introCarouselIndicators.append(
+            "<li data-target='#introCarousel' data-slide-to='" +
+              index +
+              "'></li>"
+          );
 
-    $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') + "')");
-    $(this).children('.carousel-background').remove();
-  });
+      $(this).css(
+        "background-image",
+        "url('" +
+          $(this).children(".carousel-background").children("img").attr("src") +
+          "')"
+      );
+      $(this).children(".carousel-background").remove();
+    });
 
   $(".carousel").swipe({
-    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-      if (direction == 'left') $(this).carousel('next');
-      if (direction == 'right') $(this).carousel('prev');
+    swipe: function (
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction == "left") $(this).carousel("next");
+      if (direction == "right") $(this).carousel("prev");
     },
-    allowPageScroll: "vertical"
+    allowPageScroll: "vertical",
   });
 
   // Skills section
-  $('#skills').waypoint(function () {
-    $('.progress .progress-bar').each(function () {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, {
-    offset: '80%'
-  });
+  $("#skills").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    {
+      offset: "80%",
+    }
+  );
 
   // jQuery counterUp (used in Facts section)
   $('[data-toggle="counter-up"]').counterUp({
     delay: 10,
-    time: 1000
+    time: 1000,
   });
 
   // Porfolio isotope and filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-item',
-    layoutMode: 'fitRows'
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
   });
 
-  $('#portfolio-flters li').on('click', function () {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("filter-active");
+    $(this).addClass("filter-active");
 
     portfolioIsotope.isotope({
-      filter: $(this).data('filter')
+      filter: $(this).data("filter"),
     });
   });
 
@@ -170,15 +212,15 @@
     loop: true,
     responsive: {
       0: {
-        items: 2
+        items: 2,
       },
       768: {
-        items: 4
+        items: 4,
       },
       900: {
-        items: 6
-      }
-    }
+        items: 6,
+      },
+    },
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
@@ -186,7 +228,6 @@
     autoplay: true,
     dots: true,
     loop: true,
-    items: 1
+    items: 1,
   });
-
 })(jQuery);
